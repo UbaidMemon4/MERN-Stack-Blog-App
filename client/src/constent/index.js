591e0227231d341d854adc9b9f80d3c18e436165
UbaidMemon4 <Ubaidasif510@gmail.com>
2024-07-26 12:23:01 -0700

    vercel changes

--- client/src/constent/index.js
+++ client/src/constent/index.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:8000";
 // export const BASE_URL = process.env.REACT_APP_BASE_URL;
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://mern-stack-blog-app-seven.vercel.app";
